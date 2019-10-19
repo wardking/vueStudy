12588d7625f8141f57357e9330e26f3c2821566d
ward <ward9999125@gmail.com>
2019-10-19 14:20:17 +0800

    功能完善

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="goodsinfo-container">
-       <transition
-        @before-enter="beforeEnter"
-        @enter="enter"
-        @after-enter="afterEnter"
-        >
-            <div class="ball" v-show="flag"></div>
+          <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @after-enter="afterEnter">
+            <div class="ball" v-show="ballFlag" ref="ball"></div>
         </transition>
         <!-- 商品轮播区域 -->
         <div class="mui-card">
@@ -19,10 +18,15 @@
 					<p class="price">
                         市场价:<del>￥{{goodsinfo.market_price}}</del>&nbsp;&nbsp;销售价:<span class="now_price">￥{{goodsinfo.sell_price}}</span>
                     </p>
-                    <p>购买数量：<numbox></numbox></p>
+                    <p>购买数量：<numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></numbox></p>
                     <p>
                         <mt-button type="primary" size="small">立即购买</mt-button>
                         <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
+                        <!-- 分析： 如何实现加入购物车时候，拿到 选择的数量 -->
+                        <!-- 1. 经过分析发现： 按钮属于 goodsinfo 页面， 数字 属于 numberbox 组件 -->
+                        <!-- 2. 由于涉及到了父子组件的嵌套了，所以，无法直接在 goodsinfo 页面zhong 中获取到 选中的商品数量值-->
+                        <!-- 3. 怎么解决这个问题：涉及到了 子组件向父组件传值了（事件调用机制） -->
+                        <!-- 4. 事件调用的本质： 父向子传递方法，子调用这个方法， 同时把 数据当作参数 传递给这个方法 -->
                     </p>
 				</div>
 			</div>
@@ -55,7 +59,8 @@ export default {
             id:this.$route.params.id,
             lunbotu:[],//轮播图数据
             goodsinfo:{},
-            flag:false,//控制小球的显示隐藏
+            ballFlag:false,//控制小球的显示隐藏
+            selectedCount:1,//初始化购买数量
         }
     },
     created () {
@@ -92,20 +97,41 @@ export default {
             this.$router.push({name:"goodscomment",params:{id}})
         },
         addToShopCar(){//添加到购物车
-            this.flag = !this.flag;
+            this.ballFlag = !this.ballFlag;
         },
-        beforeEnter(el){
-            el.style.transform="translate(0,0)";
+        beforeEnter(el) {
+            el.style.transform = "translate(0, 0)";
+             el.style.opacity = 1 
         },
-        enter(el,done){
+        enter(el, done) {
             el.offsetWidth;
-            el.style.transform="translate(112px, 389px)";
-            el.style.transition="all 1s cubic-bezier(.4,-0.3,1,.68)";
-            done();
+            // 小球动画优化思路：
+            // 1. 先分析导致 动画 不准确的 本质原因： 我们把 小球 最终 位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
+            // 2. 只要分辨率和 测试的时候不一样，或者 滚动条有一定的滚动距离之后， 问题就出现了；
+            // 3. 因此，我们经过分析，得到结论： 不能把 位置的 横纵坐标 直接写死了，而是应该 根据不同情况，动态计算这个坐标值；
+            // 4. 经过分析，得出解题思路： 先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，然后 让 y 值 求差， x 值也求 差，
+            // 得到 的结果，就是横纵坐标要位移的距离
+            // 5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+            
+            // 获取小球在页面中的位置
+            const ballPosition = this.$refs.ball.getBoundingClientRect();
+            // 获取徽标在页面中的位置
+            const badgePosition = document.getElementById('badge').getBoundingClientRect();
+            const xDist = badgePosition.left - ballPosition.left;
+            const yDist = badgePosition.top - ballPosition.top;
+
+            el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+            el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+            el.addEventListener('transitionend',done)
+            },
+        afterEnter(el) {
+           el.style.display="none";
+            this.ballFlag = !this.ballFlag;
         },
-        afterEnter(el){
-            this.flag = !this.flag;
-        },
+        getSelectedCount(count){
+            this.selectedCount = count;
+            // console.log("父组件拿到的数量值为"+this.selectedCount)
+        }
     },
     components: {//注册轮播图组件
         swiper,
--- src/components/subcomponents/goodsinfo_numbox.vue
+++ src/components/subcomponents/goodsinfo_numbox.vue
@@ -1,7 +1,9 @@
 <template>
-   <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+<!-- 因为异步处理的关系 max可能无法获取到值 -->
+<!-- 解决办法：可以使用watch属性来监听父组件传过来的max值 不管watch会不会被处罚，最后一次肯定是一个合法的max值-->
+   <div class="mui-numbox" data-numbox-min='1'>
 		<button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-		<input id="test" class="mui-input-numbox" type="number" value="1" />
+		<input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox" />
 		<button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
 	</div>
 </template>
@@ -12,6 +14,18 @@ export default {
         // 初始化数字选择框组件
         mui(".mui-numbox").numbox();
     },
+    methods: {
+        countChanged(){//input值发生改变时触发
+            // console.log(this.$refs.numbox.value)
+            this.$emit("getcount",parseInt(this.$refs.numbox.value))
+        }
+    },
+    props:["max"],
+    watch: {//通过属性监听判断max的值是否是合法值
+        'max':(newVal,oldVal)=>{
+            mui(".mui-numbox").numbox().setOption('max',newVal)
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
